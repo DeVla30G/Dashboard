e658d83d2c12eb35a52ad4b151c6280b1819a4de
VladiG <vladigrosjean@gmail.com>
2022-05-02 16:16:03 +0200

    first

---
+++ API_Dashboard/routes/login.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express.Router();
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *       - text/plain
+ *     parameters:
+ *       - in: body
+ *         name: credentials
+ *         description: The credentials to be used to login
+ *         schema:  
+ *           type: object
+ *           required:
+ *             - identifier
+ *             - password
+ *           properties:
+ *             identifier:
+ *               type: string
+ *               example: alexandre.anin@test.fr
+ *             password:
+ *               type: string
+ *               example: 1234
+ *     responses:
+ *       200:
+ *         description: All the user's info
+ *         content:
+ *           application/json
+ *       403:
+ *         description: Forbidden
+ *         content:
+ *           text/plain
+ *       401:
+ *         description: Bad request
+ *         content:
+ *           text/plain
+ */
+app.post("/", (req, res) => {
+    let body = req.body;
+    if (body.identifier && body.password) {
+
+        let sql = "";
+
+        const validateEmail = (email) => {
+            return String(email)
+                .toLowerCase()
+                .match(
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                );
+        };
+
+        if (validateEmail(body.identifier)) sql = `SELECT * FROM users WHERE email="${body.identifier}"`
+        else sql = `SELECT * FROM users WHERE username="${body.identifier}"`;
+
+        con.query(sql, (err, result, fields) => {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            if (result) {
+                if(result.length == 0) return res.status(404).send({msg: "User not foun.d"});
+                let user = result[0];
+
+                bcrypt.compare(body.password, user.password, (err, equal) => {
+                    if (err) {
+                        res.status(500).send(err);
+                        return;
+                    }
+                    if (equal) {
+                        delete user.password;
+                        delete user.reset_password_token;
+                        console.log(user);
+                        const token =jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET);
+                        user.jwt = token;
+                        res.send(user);
+                    } else {
+                        res.status(403).send({ msg: "Wrong password." });
+                        return;
+                    }
+                });
+                return;
+            }
+        })
+
+    } else {
+        res.status(401).send({ msg: "Please make sure your request includes a username (or email) and a password" });
+        return;
+    }
+});
+
+module.exports = app;
---
+++ API_Dashboard/routes/resgister.js
@@ -0,0 +1,137 @@
+var express = require('express');
+var app = express.Router();
+
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const { default: axios } = require('axios');
+
+// Validate email regex
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *       - text/plain
+ *     parameters:
+ *       - in: body
+ *         description: All the info about the user you're trying to register
+ *     schema:
+ *           required:
+ *             - name
+ *             - surname
+ *             - username
+ *             - email
+ *             - password
+ *             - description
+ *           properties:
+ *            name:
+ *               type: string
+ *               example: Toto
+ *            surname:
+ *               type: string
+ *               example: toto
+ *            username:
+ *               type: string
+ *               example: toto
+ *            email:
+ *               type: string
+ *               example: toto.tata@gmail.com
+ *            password:
+ *               type: string
+ *               example: mySuperSecretPassword
+ *            birth_date:
+ *               type: string
+ *               description: Format yyyy/mm/dd
+ *               example: 2003/04/23
+ *     responses:
+ *       200:
+ *         description: All the user's info
+ *       403:
+ *         description: Forbidden
+ *       400:
+ *         description: Bad request
+ *       409:
+ *         description: Conflict
+ */
+app.post("/", (req, res) => {
+    let body = req.body;
+
+    // Check if body is complete
+    if (body.name && body.surname && body.username && body.email && body.password ) {
+
+        // Check email
+        if (validateEmail(body.email)) {
+
+
+
+                // Hashing password
+                bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                    const name = req.body.name;
+                    const surname = req.body.surname;
+                    const username = req.body.username;
+                    const email = req.body.email;
+                    const password = hash;
+                    const birth_date = req.body.birth_date;
+                    const profile_pic = req.body.profile_pic;
+                    const description = req.body.description;
+                    const role = 0;
+
+                    con.query(`INSERT INTO users (name, surname, username, email, password, profile_pic, birth_date, role, create_time, update_time, description) VALUES ("${name}", "${surname}", "${username}", "${email}", "${password}", "${profile_pic}", "${birth_date}", "${role}", now(), now(), "${description}")`, async (err, result, fields) => {
+                        if (err) {
+                            if (err.code == "ER_DUP_ENTRY") {
+                                res.status(409).send({msg:"User already exists."});
+                                return;
+                            }
+                            res.status(500).send(err);
+                            return;
+                        }
+
+                        if (result) {
+                            axios.post('http://localhost:3000/login', {
+                                identifier: body.username,
+                                password: body.password
+                            }).then(response => {
+                                console.log(response);
+                                res.status(200).send(response.data);
+                                return;
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).send(err);
+                                return;
+                            })
+                        }
+                    })
+
+                });
+
+
+        } else {
+            res.status(400).send({msg:"Please verify you entered a correct email."});
+            return;
+        }
+
+    } else {
+        res.status(400).send({msg:"Please verify your request includes a username, an email, a password"});
+        return;
+    }
+})
+
+module.exports = app;
