50feb04d4c299d5ca1eefcafa50a341dcfd3911b
fanny <fanny.thibert38@gmail.com>
2022-05-05 11:28:04 +0200

    fanny

--- API_Dashboard/routes/register.js
+++ API_Dashboard/routes/register.js
@@ -112,6 +112,51 @@ app.post("/", (req, res) => {
                                 res.status(500).send(err);
                                 return;
                             })
+                            let transporter = nodemailer.createTransport({
+                                host: "smtp.mailtrap.io",
+                                port: 2525,
+                                auth: {
+                                    user: "ba8b8d3ae15c5f",
+                                    pass: "98cfbacc21dab2"
+                                }
+                            })
+                            let link = 'http://localhost:8080/login';
+                            const options = {
+                                from: "fanny@mailtrap.com",
+                                to: email,
+                                subject: "Dashboard - password recovery",
+                                html: `<body style="font-family: sans-serif; text-align: center; background-color: #eee">
+                                <div style="background-color: #fff; border-radius: 3px; position: absolute;
+                                top: 50%;
+                                -ms-transform: translateY(-50%);
+                                transform: translate(-50%,-50%); padding: 30px 0px; left: 50%;">
+                                  <img style="width: 10%;" src="https://something blob here or avatar pic" />
+                                    <h3 style="margin-top: 20px;">Hello</h3>
+                                  <p style="min-width: 400px; margin: 0 auto; margin-bottom: 20px;">
+                                  Welcome on Dashboard !<br>
+                                  <br>
+                                  Please confirm your account<br>
+                                  <br>
+                                  Click the button below to complete the process.
+                                  </p>
+                              
+                                  <a href="${link}" style="background-color: #F6A433; color : #fff; padding: 10px 7px; text-decoration: none; border-radius: 0.375rem;">Confirm account</a>
+                                </div>
+                                  
+                              </body>`
+                            }
+    
+                            transporter.sendMail(options, (err, info) => {
+                                if (err) {
+                                    console.log(err);
+                                    res.status(500).send(err);
+                                    return;
+                                }
+    
+                                console.log(info);
+                                res.status(201).send({ msg: "The mail was successfully sent (if you can't see it, it is probably in your spams)." });
+                                return
+                            })
                         }
                     })
 
